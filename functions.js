3ef021bb527a68690af4cffcd0693bd2021618c0
Sol1323 <sbeniquez084@eap.edu>
2016-11-27 15:26:18 -0800

    Completed exercise #1

--- functions.js
+++ functions.js
@@ -1,1 +1,26 @@
-// TODO: add your solutions here!
+
+//1. Build your own concatenation
+
+function combineWords(word1, word2) {
+  var newString = word1.concat(word2);
+  return newString;
+}
+
+var result = combineWords('dog', 'house');
+console.log(result);
+// displays 'doghouse'
+
+
+// Display an argument phrase to the console n times
+
+function repeatPhrase(phrase, n) {
+    // TODO: Place your code here
+}
+
+repeatPhrase("Hello", 5);
+// displays
+// Hello
+// Hello
+// Hello
+// Hello
+// Hello
